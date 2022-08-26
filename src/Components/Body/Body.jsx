aad71eece47bf9c044841a69f67f6f6adf925161
Akash Borate <27akashborate@gmail.com>
2022-08-26 17:21:21 +0530

    body structure

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import "./Body.scss";
 
 const Body = () => {
-  return <div>Body</div>;
+  return (
+    <div className="Main_Body">
+      <div className="Body_Nav">
+        <h4>BMX</h4>
+        <h4>Mountain</h4>
+        <h4>Road</h4>
+        <h4>Track</h4>
+      </div>
+      <div className="blank"></div>
+      <h2 className="Highlights">Highlights</h2>
+      <div className="Sport_Pics">
+        <div className="Pics">
+          <img
+            className="Images"
+            src="https://images.unsplash.com/photo-1554201267-8eb0907c5d9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ym14fGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+            alt=""
+          />
+        </div>
+        <div className="Pics">
+          <img
+            className="Images"
+            src="https://images.unsplash.com/photo-1542715339-83599a9f0432?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Ym14fGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+            alt=""
+          />
+        </div>
+        <div className="Pics">
+          <img
+            className="Images"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1UvOYQK8PZUvPOHYCp_j3MRl7SYlAAdcBROyNqQjSU96Zo8IghisEtYxHAhYdynTmdmo&usqp=CAU"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Body;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -18,30 +18,39 @@ const Navbar = () => {
     <div className="Navbar">
       <div className="NavIcons">
         <FontAwesomeIcon icon={faStar} className="Icons" />
+        <h5 className="Sport">Featured</h5>
       </div>
       <div className="NavIcons">
         <FontAwesomeIcon icon={faChildRifle} className="Icons" />
+        <h5 className="Sport">Armoury</h5>
       </div>
       <div className="NavIcons">
         <FontAwesomeIcon icon={faPersonSwimming} className="Icons" />
+        <h5 className="Sport">Aquatics</h5>
       </div>
       <div className="NavIcons">
         <FontAwesomeIcon icon={faPersonBiking} className="Icons" />
+        <h5 className="Sport">Cycling</h5>
       </div>
       <div className="NavIcons">
         <FontAwesomeIcon icon={faHorse} className="Icons" />
+        <h5 className="Sport">Equestrain</h5>
       </div>
       <div className="NavIcons">
         <FontAwesomeIcon icon={faPersonSkating} className="Icons" />
+        <h5 className="Sport">Extreme</h5>
       </div>
       <div className="NavIcons">
         <FontAwesomeIcon icon={faGolfBallTee} className="Icons" />
+        <h5 className="Sport">Golf</h5>
       </div>
       <div className="NavIcons">
         <FontAwesomeIcon icon={faChildReaching} className="Icons" />
+        <h5 className="Sport">Gymnastics</h5>
       </div>
       <div className="NavIcons">
         <FontAwesomeIcon icon={faWater} className="Icons" />
+        <h5 className="Sport">Lake & Sea</h5>
       </div>
     </div>
   );
